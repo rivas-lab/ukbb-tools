2dcae52bfbc6ac204d1c21c9a1edfa4074d8b7ef
Matthew Aguirre <magu@sh-102-07.int>
2018-11-07 13:39:16 -0800

    First functional gwas script (??? first operational test in progess)

--- gwas/gwas.py
+++ gwas/gwas.py
@@ -8,17 +8,28 @@ Author: Matthew Aguirre (SUNET: magu)
 
 def make_plink_command(bpFile, pheFile, outFile, pop, related=False, plink1=False, arrayCovar=False):
     # paths to plink genotypes, input phenotypes, output directory are passed
-    # paths to qc files (ethnic groups, related individual list) are coded here
+    # paths to qc files (covars, ethnic groups, related individuals, ) are coded here
     covarFile='/oak/stanford/groups/mrivas/ukbb24983/sqc/ukb24983_GWAS_covar.phe'
+    # make sure this includes most recent update of redacted individuals!
+    popFile='/oak/stanford/groups/mrivas/ukbb24983/sqc/population_stratification/ukb24983_{0}.phe'.format(pop)
+    unrelatedFile='/oak/stanford/groups/mrivas/ukbb24983/sqc/ukb24983_v2.used_in_pca.phe' if not related else ''
+    # infer whether we need to subset variant lists to have array as a covariate 
+    # (only true for genotyped runs)
+    if arrayCovar and '/cal/' in bpFile: 
+        arrayVarFile='/oak/stanford/groups/mrivas/ukbb24983/sqc/both_array_variants.txt'
+    if not arrayCovar and '/cal/' in bpFile:
+        arrayVarFile='/oak/stanford/groups/mrivas/ukbb24983/sqc/one_array_variants.txt'
+    else:
+        arrayVarFile=''
+    # paste together the command from constituent parts
     return " ".join(["plink" if plink1 else "plink2", 
-                     "--bfile" if plink1 else "--pfile", bpFile,
+                     "--bfile", bpFile,
                      "--pheno", pheFile, "--pheno-quantile-normalize",
                      "--glm firth-fallback hide-covar",
-                     "--keep /path/to/ethnic/group",
-                     "--exclude /latest/redacted.list /path/to/related/inds.list",
-                     "--extract {0}".format("on_both.arrays" if not arrayCovar else "on_one.array"),
+                     "--keep", popFile if pop != 'all' else "", unrelatedFile,
+                     "--extract", arrayVarFile,
                      "--covar", covarFile, 
-                     "--covar-name age sex {0} PC1-PC4".format("Array" if arrayCovar else ""), 
+                     "--covar-name age sex", "Array" if arrayCovar else "", "PC1-PC4",
                      "--out {0}".format(outFile)]) 
 
 def make_batch_file(batchFile, plinkCmd, memory, time, partitions):
@@ -26,7 +37,7 @@ def make_batch_file(batchFile, plinkCmd, memory, time, partitions):
        f.write("\n".join(["#!/bin/bash","",
                           "#SBATCH --job-name=RL_GWAS",
                           "#SBATCH --output={}".format(os.path.join(os.path.dirname(batchFile), "rl-gwas_%A-%a.out")),
-                          "#SBATCH --memory={}".format(memory),
+                          "#SBATCH --mem={}".format(memory),
                           "#SBATCH --time={}".format(time),
                           "#SBATCH -p {}".format(','.join(partitions)),
                           "", plinkCmd]))
@@ -47,10 +58,13 @@ def run_gwas(kind, pheFile, outDir='', pop='white_british', related=False, plink
     if pop not in ['all', 'white_british', 'african', 's_asian', 'e_asian']:
         raise ValueError("population must be one of (all, white_british, african, s_asian, e_asian)")
     # derive for the below
-    imp_bfile_path='imp.pgen'
-    cal_bfile_path='cal.pgen'
-    outFile='out'
+    imp_bfile_path='/oak/stanford/groups/mrivas/private_data/ukbb/24983/imp/pgen/ukb_imp_chr${SLURM_ARRAY_TASK_ID}_v2.mac1.hrc'
+    cal_bfile_path='/oak/stanford/groups/mrivas/private_data/ukbb/24983/cal/pgen/ukb24983_cal_cALL_v2'
+    # TODO: systematically name the output
+    pheName=os.path.basename(pheFile).split('.')[0]
+    outFile='ukb24983_v2.{0}.{1}'.format(pheName, kind)
     if kind == 'imputed':
+        outFile += '.chr${SLURM_ARRAY_TASK_ID}'
         # make this an array job with nJobs=22
         cmd = make_plink_command(bpFile  = imp_bfile_path,
                                  pheFile = pheFile,
@@ -60,37 +74,38 @@ def run_gwas(kind, pheFile, outDir='', pop='white_british', related=False, plink
                                  plink1  = plink1,
                                  arrayCovar = True)
     elif kind == 'genotyped': 
-        # need a run with array a covariate, and one without
+        # need one plink call with array a covariate, and one without
+        outFile1 = outFile+'.both_arrays'
         cmd1 = make_plink_command(bpFile  = cal_bfile_path,
                                   pheFile = pheFile,
-                                  outFile = outFile,
+                                  outFile = outFile1,
                                   pop     = pop,
                                   related = related,
                                   plink1  = plink1,
                                   arrayCovar = True)
+        outFile2 = outFile+'.one_array'
         cmd2 = make_plink_command(bpFile  = cal_bfile_path,
                                   pheFile = pheFile,
-                                  outFile = outFile,
+                                  outFile = outFile2,
                                   pop     = pop,
                                   related = related,
                                   plink1  = plink1,
                                   arrayCovar = False)
-        cmd = "\n".join([cmd1, cmd2, # this is the plink part, below joins the two files
-                         "cat out1 out2 | sort -k1,1n -kN,lookItUp > finalout",
-                         "cat out1.log out2.log > finalout.log",
-                         "rm out1.* out2.*" # this is problematic if one is a substring, so program 1/2 yourself to avoid
+        # TODO: make sure that the plink output is actually getting formatted like this
+        # join the plink calls, add some bash at the bottom to combine the output
+        cmd = "\n\n".join([cmd1, cmd2, # this is the plink part, below joins the two files
+                         "cat {0}*.hybrid {1}*.hybrid | sort -k1,1n -k2,2n > {2}.glm.hybrid".format(outFile1, outFile2, outFile),
+                         "cat {0}.log {1}.log > {2}.log".format(outFile1, outFile2, outFile),
+                         "rm {0}.* {1}.*".format(outFile1, outFile2),
                         ])
     else:
         raise ValueError("argument kind must be one of (imputed, genotyped): {0} was provided".format(kind))
-    sbatch = make_batch_file(batchFile = os.path.join(logDir, "some_name_{}.sh".format(kind)),
+    sbatch = make_batch_file(batchFile = os.path.join(logDir, "gwas.{0}.{1}.sbatch.sh".format(kind,pheName)),
                              plinkCmd  = cmd,
                              memory    = memory,
                              time      = time,
                              partitions = partition)
-    return
-    os.system(" ".join("sbatch", 
-                       "{0} -o {1}".format("--array=1-22" if imputed else "", outDir),
-                       sbatch))
+    os.system(" ".join(("sbatch", "--array=1-22" if kind == 'imputed' else "", "-o", outDir, sbatch))) 
 
 
 if __name__ == "__main__":
@@ -109,17 +124,17 @@ if __name__ == "__main__":
                             help='Path to phenotype file(s)')
     parser.add_argument('--out', dest="outDir", required=True, nargs=1,
                             help='Path to desired output *directory*. Summary stats will be output according to phenotype name (derived from passed file) and Rivas Lab specification for GBE. Defaults to current working directory.')
-    parser.add_argument('--population', dest="pop", required=False, default="white_british", nargs=1,
+    parser.add_argument('--population', dest="pop", required=False, default=["white_british"], nargs=1,
                             help='Flag to indicate which ethnic group to use for GWAS. Must be one of all, white_british, e_asian, s_asian, african')
     parser.add_argument('--keep-related', dest="relatives", action='store_true',
                             help='Flag to keep related individuals in GWAS. Default is to remove them.')
-    parser.add_argument('--batch-memory', dest="sb_mem", required=False, default="16000", nargs=1,
+    parser.add_argument('--batch-memory', dest="sb_mem", required=False, default=["16000"], nargs=1,
                             help='For underlying batch job submission: Amount of memory (in MB) to request. Default is 16000.')
-    parser.add_argument('--batch-time', dest="sb_time", required=False, default="24:00:00", nargs=1,
+    parser.add_argument('--batch-time', dest="sb_time", required=False, default=["24:00:00"], nargs=1,
                             help='For underlying batch job submission: Amount of time (DD-HH:MM:SS) to request. Default is 24 hours.')
     parser.add_argument('--batch-partitions', dest="sb_parti", required=False, default=["normal","owners"], nargs='*',
                             help='For underlying batch job submission: Compute partition to submit jobs. Default is normal,owners.')
-    parser.add_argument('--log-dir', dest="log", required=False, nargs=1,
+    parser.add_argument('--log-dir', dest="log", required=False, nargs=1, default=[''],
                             help="Directory in which to place log files from this script and its related SLURM jobs. Default is current working directory.")
     args = parser.parse_args()
     # feature add: genotype model  
