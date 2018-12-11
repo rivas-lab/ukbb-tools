7e35562a56fcd0b47bd528e8d3d8a21032740f64
Matthew Aguirre <magu@sh-ln04.stanford.edu>
2018-12-10 21:14:57 -0800

    Initial commit for master phenotype dile creation and phewas

--- phewas/README.md
+++ phewas/README.md
@@ -1,1 +1,16 @@
-hi
+## PheWAS
+
+This directory contains scripts for running PheWAS (`phewas.py`) and generating the files necessary to do so (`combine_phe.py`).
+
+# Dependencies:
+
+_Note_: this section only needs to be updated when new phenotype data comes in, but feel free to read it for more info about where the files used by `phewas.py` come from.
+
+In order to run PheWAS, we need a master table containing all the phenotype data we've processed (see the [phenotyping](https://github.com/rivas-lab/ukbb-tools/blob/master/phenotyping/) folder for more info). To do this, we leverage the phenotype info table, which is also in the phenotyping folder. The maker script, `combine_phe.py` will walk through this file and selecting phenotypes with n/N > 100 for inclusion. It then loads each of those files into memory (!!) via a dictionary keyed on sample ID, then writes all the data out to file. 
+
+If you're rerunning this, you'll probably want a lot of memory -- 64g is suffient in my experience (Matt).
+
+
+# Analysis:
+
+This script is under construction. 
---
+++ phewas/phewas.py
@@ -0,0 +1,141 @@
+#!/bin/bash
+import os
+import glob
+
+# useful
+os.system('ml load plink2')
+_README_ = '''
+A script to run phewas. More documentation forthcoming.
+
+Author: Matthew Aguirre (SUNET: magu)
+'''
+
+# Input: gene name(s) / variant names(s) / region
+# variant names for imputed data are chrom:pos:ref:alt
+
+cal_bims = glob.glob('/oak/stanford/groups/mrivas/ukbb24983/cal/pgen/ukb24983_cal_chr*_v2.bim')
+imp_bims = glob.glob('/oak/stanford/groups/mrivas/ukbb24983/imp/pgen/ukb_imp_chr*_v2.mac1.hrc.bim')
+
+
+# 1. Get all variants in gene (if applicable)
+def find_variants(chrom,start,end):
+    # helper
+    overlaps = lambda line,start,end: int(start) <= int(line.split()[3]) and int(line.split()[3]) <= int(end)
+    var_list = {} # file:[list of variant ids]
+    for bim in filter(lambda x:'chr'+chrom in x, cal_bims + imp_bims):
+        with open(bim, 'r') as f:
+            var_list[bim[:-4]] = [line.split()[1] for line in f if overlaps(line,start,end)] 
+    return var_list
+
+def find_named_variants(var_list):
+    file_map = {} # this is the same as var_list in find_variants()
+    for bim in cal_bims + imp_bims:
+        with open(bim, 'r') as f:
+            # find variants, update list of variants to find
+            file_map[bim[:-4]] = [var for var in var_list for line in f if var == line.split()[1]]
+            var_list = [var for var in var_list if var not in file_map[bim[:-4]]]
+    if len(var_list) > 0:
+        print("Could not find the following variants:\n{}".format("\n".join(var_list)))
+        print("Note: variant names for imputed data are formatted CHR:POS_REF_ALT !")
+    return {k:v for k,v in file_map.items() if len(v) > 0}    
+
+            
+def get_variants(gene):
+    with open('genes_hg19.txt', 'r') as f:
+        for line in f:
+            if gene == line.rstrip().split()[-1]:
+                c,bp1,bp2 = line.split()[:3]
+                return find_variants(c,bp1,bp2)
+    raise ValueError("Could not find gene: {}".format(gene))
+
+# 2. Run PheWAS(es)
+def run_phewas(bfile, var_ids, out_prefix):
+    master_phe = '/oak/stanford/groups/mrivas/dev-ukbb-tools/phewas/resources/master.phe'
+    covars     = '/oak/stanford/groups/mrivas/ukbb24983/sqc/ukb24983_GWAS_covar.phe'
+    # write out temp file for variants
+    with open(out_prefix + '.varlist.txt', 'w') as o:
+        o.write('\n'.join(var_ids))
+    # do the thing
+    os.system(' '.join(['plink2 --bfile', bfile, '--pheno', master_phe, '--out', out_prefix, 
+                                   '--covar', covars, '--covar-name age sex Array PC1-PC4',
+                                   '--extract', out_prefix + '.varlist.txt', 
+                                   '--glm firth-fallback hide-covar']))
+    return
+     
+
+# 3. Combine results
+def combine_output(common_prefix, out_prefix, keep_na=False):
+    # do qts and bins separately
+    for is_bin, ending in enumerate(['linear', 'logistic.hybrid']):
+        files = glob.glob(common_prefix + '*' + ending)
+        print(len(files))
+        with open(out_prefix + 'phewas.glm.' + ending, 'w') as o:
+            for n,f in enumerate(files):
+                pheno = os.path.basename(f).split('.')[-3 - is_bin]
+                with open(f, 'r') as i:
+                    for j,line in enumerate(i):
+                        if n == 0 and j == 0:
+                            o.write('#PHENO\t' + line[1:])
+                        elif j > 0 and (keep_na or line.split()[-1] != 'NA'):
+                            o.write(pheno + '\t' + line)
+    return
+
+
+# ok now let's actually do the darn thing:
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser( formatter_class=argparse.RawDescriptionHelpFormatter,
+                                          description=_README_ )
+    parser.add_argument('--gene', dest="gene", required=False, default = [], nargs=1,
+                            help='input gene for phewas')
+    parser.add_argument('--region', dest="cpra", required=False, default = [], nargs=1,
+                            help='input region for phewas (format: CHROM:BP1-BP2')
+    parser.add_argument('--variants', dest="vars", required=False, default = [], nargs='*', 
+                            help='input variant ID(s) for phewas (can also be a file, with one variant ID per line)')
+    parser.add_argument('--out', dest="out", required=True, default = ['phewas'], nargs=1,
+                            help='input region for phewas')
+    args = parser.parse_args()
+    print(args)
+    
+    # ensure usage
+    if sum(map(lambda x: not x, (args.gene, args.cpra, args.vars))) < 2:
+        raise ValueError("Only one of --gene, --region, --variants, can be supplied.")
+    
+    # get variants
+    if args.gene:
+        bfile_to_vars = get_variants(args.gene[0])
+    elif args.cpra:
+        chrom, bps = args.cpra[0].split(':')
+        bp1, bp2   = bps.split('-')
+        bfile_to_vars = find_variants(chrom,bp1,bp2)
+    elif args.vars:
+        if os.path.isfile(args.vars[0]):
+            with open(args.vars[0], 'r') as f:
+                vs = [line.rstrip().split()[0] for line in f]
+            print("{} variant IDs found in input file.".format(len(vs)))
+        else:
+            vs = args.vars
+        bfile_to_vars = find_named_variants(vs)
+    else:
+        raise ValueError("One of --gene, --region, --variants, must be supplied!")
+    
+    # run phewas
+    print("{} variants found from specified input.".format(sum(map(len, bfile_to_vars.values()))))
+
+    if sum(map(len,bfile_to_vars.values())) >= 100:
+        print("WARNING: attempting phewas with > 100 variants! This will probably take awhile, and might not finish...")
+
+    print("Running phewas...")
+    
+    # give these runs a random hash to prevent wrong files from being included when we join results
+    import random
+    temp_out = args.out[0] + '.' + str(int(1000000 * random.random()))
+    for bfile,variants in filter(lambda x: len(x[1]) > 0, bfile_to_vars.items()):
+        run_phewas(bfile, variants, temp_out)
+    
+    # now combine those files into the final result
+    combine_output(temp_out, args.out[0], keep_na = False)
+    
+    # and remove the middling files
+    os.system("rm {}*".format(temp_out))
+    # done    
