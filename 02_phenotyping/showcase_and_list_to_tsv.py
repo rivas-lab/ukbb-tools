b610a5cba186bf96a455cf806db99cf344939bec
Yosuke Tanigawa <yk-tanigawa@users.noreply.github.com>
2019-03-27 22:18:47 +0900

    Update showcase_and_list_to_tsv.py
    Refactor code.

--- 02_phenotyping/showcase_and_list_to_tsv.py
+++ 02_phenotyping/showcase_and_list_to_tsv.py
@@ -1,35 +1,69 @@
-import pandas as pd
+_README='read in list of fields, subset from big csv.'
+
+
 import argparse
 
-dds = pd.read_csv('Data_Dictionary_Showcase.csv')
 
-#Parse args to get the csv of fields
-parser = argparse.ArgumentParser(description='read in list of fields, subset from big csv.')
-parser.add_argument('csvfile', type=argparse.FileType('r'), help='Input csv file')
-args = parser.parse_args()
+import pandas as pd
 
-filename = args.csvfile
 
-#Read in the fields
-fields = pd.read_csv(filename, header=None, names=['FieldID'])
+def join_and_add_cols(filename, ref):
+    dds = pd.read_csv(ref)
 
-#Subset the df with a merge
-subsetted = fields.merge(dds, left_on='FieldID', right_on='FieldID')
+    #Read in the fields
+    fields = pd.read_csv(filename, header=None, names=['FieldID'])
 
-#Add in the extra column names
-add_colnames = ['Annotator', 'Annotation date', 'Name', 'GBE ID', 'TableID', 'Field', 'QT_total_num', 'BIN_total_num', 'QT_index', 'BIN_index', 'coding_exclude', 'coding_QT', 'coding_binary_case', 'coding_binary_control']
-subsetted = pd.concat([subsetted,pd.DataFrame(columns=add_colnames)], sort=True)
+    #Subset the df with a merge
+    subsetted = fields.merge(dds, left_on='FieldID', right_on='FieldID')
 
-#Reorder the columns
-new_col_order = ['Annotator', 'Annotation date', 'Name', 'GBE ID', 'TableID', 'Field', 'FieldID', 'QT_total_num', 'BIN_total_num', 'QT_index', 'BIN_index', 'coding_exclude', 'coding_QT', 'coding_binary_case', 'coding_binary_control', 'Participants', 'Stability', 'ValueType', 'Units', 'Strata', 'Sexed', 'Instances', 'Array', 'Coding', 'Link']
-subsetted = subsetted[new_col_order]
+    #Add in the extra column names
+    add_colnames = ['Annotator', 'Annotation date', 'Name', 'GBE ID', 'TableID', 'Field', 'QT_total_num', 'BIN_total_num', 'QT_index', 'BIN_index', 'coding_exclude', 'coding_QT', 'coding_binary_case', 'coding_binary_control']
+    subsetted = pd.concat([subsetted,pd.DataFrame(columns=add_colnames)], sort=True)
 
-#ensure types of columns are int
-subsetted = subsetted.astype({"FieldID": int, "Participants": int, "Instances": int, "Array": int})
+    #Reorder the columns
+    new_col_order = ['Annotator', 'Annotation date', 'Name', 'GBE ID', 'TableID', 'Field', 'FieldID', 'QT_total_num', 'BIN_total_num', 'QT_index', 'BIN_index', 'coding_exclude', 'coding_QT', 'coding_binary_case', 'coding_binary_control', 'Participants', 'Stability', 'ValueType', 'Units', 'Strata', 'Sexed', 'Instances', 'Array', 'Coding', 'Link']
+    subsetted = subsetted[new_col_order]
 
-outfilename = filename.name[:-3] + 'tsv'
+    #ensure types of columns are int
+    subsetted = subsetted.astype({"FieldID": int, "Participants": int, "Instances": int, "Array": int})
+    
+    return(subsetted)
 
-print(outfilename)
+  
+def showcase_and_list_to_tsv_main():
+    #Parse args to get the csv of fields
+    parser = argparse.ArgumentParser(description=_README)
+    parser.add_argument(
+      'csvfile', 
+      type=argparse.FileType('r'), 
+      help='Input csv file'
+    )
+    parser.add_argument(
+      '--ref', type=argparse.FileType('r'), 
+      help='Data_Dictionary_Showcase.csv',
+      default='Data_Dictionary_Showcase.csv'
+    )
+    parser.add_argument(
+      '--out', 
+      help='output tsv file (default: ${csvfile%.csv}.tsv)',
+      default=None
+    )
+    
+    args = parser.parse_args()
 
-#Export to new file
-subsetted.to_csv(outfilename, sep='\t', index=False)
+    filename = args.csvfile
+    ref = args.ref
+    if args.out is not None:
+        outfilename = args.out
+    else:
+        outfilename = filename.name[:-3] + 'tsv'
+
+    subsetted = join_and_add_cols(filename, ref)
+    
+    print(outfilename)
+    #Export to new file
+    subsetted.to_csv(outfilename, sep='\t', index=False)
+
+    
+if __name__ == '__main__':
+    showcase_and_list_to_tsv_main()
