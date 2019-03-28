1a68804fd049aa5a8182cca334da9477dd1c93ff
Matthew Aguirre <magu@sh-ln06.stanford.edu>
2019-03-27 17:31:18 -0700

    put back quantile normalization

--- 04_gwas/gwas.py
+++ 04_gwas/gwas.py
@@ -17,7 +17,7 @@ def make_plink_command(bpFile, pheFile, outFile, pop, related=False, plink1=Fals
     # paste together the command from constituent parts
     return " ".join(["plink" if plink1 else "plink2", 
                      "--bfile" if plink1 else "--bpfile", bpFile, "--chr 1-22",
-                     "--pheno", pheFile, #"--pheno-quantile-normalize",
+                     "--pheno", pheFile, "--pheno-quantile-normalize",
                      "--glm firth-fallback hide-covar omit-ref",
                      "--keep {0}".format(popFile) if popFile else "", 
                      "--remove {0}".format(unrelatedFile) if unrelatedFile else "",
