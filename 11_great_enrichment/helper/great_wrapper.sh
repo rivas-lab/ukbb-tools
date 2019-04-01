f96b3aecfe5b696dfda567333b84e011a25db4ca
Yosuke Tanigawa <ytanigaw@stanford.edu>
2019-04-01 03:02:09 -0700

    add scripts for enrichment analysis

--- 11_great_enrichment/helper/great_wrapper.sh
+++ 11_great_enrichment/helper/great_wrapper.sh
@@ -5,20 +5,15 @@ usage () {
     echo "usage: $0 in.bed assembly out.dir"
 }
 
-get_onto_list () {
-    assembly=$1
-    onto_file="$(dirname $(dirname $(readlink -f $0)))/misc/great.ontology.${assembly}.20171029.txt"
-
-    cat ${onto_file} | egrep -v '^#' | cut -f1 | tr "\n" "," | rev | cut -c2- | rev
-}
+source $(dirname $(readlink -f $0))/great_misc_func.sh
 
 if [ $# -lt 3 ] ; then usage >&2 ; exit 1 ; fi
 in_file=$1
 assembly=$2
 out_d=$3
 
-get_onto_list $assembly
-
-exit 0
-GREATER 
+GREATER \
+    --requiredTests=neither \
+    --ontologies=$( get_onto_list $assembly ) \
+    $assembly $in_file $out_d
 
