a9fa59150e00e7d898cc3ec8f122abda2e92b055
Yosuke Tanigawa <ytanigaw@stanford.edu>
2019-04-03 13:32:15 -0700

    update bulk DL scripts

--- 08_bulk_DL/ukbfetch_bulk_wrapper.sh
+++ 08_bulk_DL/ukbfetch_bulk_wrapper.sh
@@ -2,14 +2,17 @@
 set -beEuo pipefail
 
 bulk_file=$1
-out_dir=$(readlink -f $2)
+out_dir=$(  readlink -f $2 )
+key_file=$( readlink -f $3 )
+
+if [ $# -gt 3 ] ; then num_jobs=$4 ; else num_jobs=8 ; fi
 
 # create a temp directory
-tmpDir=$(mktemp -d -p $LOCAL_SCRATCH tmp-$(basename $0)-$(date +%Y%m%d-%H%M%S)-XXXXXXXXXX) ; echo "temp_dir = $tmpDir"
+tmpDir=$( mktemp -d -p $LOCAL_SCRATCH tmp-$( basename $0 )-$( date +%Y%m%d-%H%M%S )-XXXXXXXXXX ) ; echo "temp_dir = $tmpDir" >&2
 handler_exit () { rm -rf $tmpDir ; }
-trap handler_exit EXIT
+# trap handler_exit EXIT
 
-bulk_file_missing=${tmpDir}/$(basename $bulk_file)-$(date +%Y%m%d-%H%M%S)
+bulk_file_missing=${tmpDir}/$( basename $bulk_file )-$( date +%Y%m%d-%H%M%S )
 
 find ${out_dir} -type f \
 | awk -v FS='/' '{print $NF}' \
@@ -18,10 +21,10 @@ find ${out_dir} -type f \
 | comm -1 -3 /dev/stdin <( cat ${bulk_file} | awk -v OFS="_" '{print $1, $2}'| sort -k1 ) \
 | awk -v FS='_' -v OFS='_' '{print $1 " " $2, $3, $4}' > ${bulk_file_missing}
 
-n_lines=$(cat ${bulk_file_missing} | wc -l)
+n_lines=$( cat ${bulk_file_missing} | wc -l )
 n_batches=$( perl -e "print(int((${n_lines} + 999)/1000))" )
 
 echo "Downloading ${n_lines} files (${bulk_file_missing})"
 
-parallel -j 4 $(dirname $(readlink -f $0))/$(basename $0 .sh)_sub.sh ${bulk_file_missing} {} ${out_dir} :::  $(seq 1 ${n_batches}) 
+parallel -j ${num_jobs} $(dirname $(readlink -f $0))/$(basename $0 .sh)_sub.sh ${bulk_file_missing} {} ${out_dir} ${key_file} :::  $(seq 1 ${n_batches}) 
 
