66c5875cca900302e98ab432bff0b8462a8cde6f
Guhan Ram Venkataraman <guhan@sh-ln03.stanford.edu>
2019-04-08 14:32:59 -0700

    moved around some file

---
+++ 02_phenotyping/extras/convert_to_24983.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+# replace dir in ls below with desired directory to convert to 24983 from 16698
+# replace python script path if not proper
+for f in $(ls /oak/stanford/groups/mrivas/ukbb24983/phenotypedata/extras/rohit/phe); do
+    python /oak/stanford/groups/mrivas/users/$USER/repos/ukbb-tools/02_phenotyping/extras/from_16698_to_24983.py $f to24983$f
+    mv to24983$f $f
+done
