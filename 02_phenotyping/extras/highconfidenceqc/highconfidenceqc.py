1a33f9ce09747a1afa2259ff86b5c2325d464873
Guhan Ram Venkataraman <guhan@sh-ln03.stanford.edu>
2019-04-08 16:19:47 -0700

    standardized extras folder

--- 02_phenotyping/extras/highconfidenceqc/highconfidenceqc.py
+++ 02_phenotyping/extras/highconfidenceqc/highconfidenceqc.py
@@ -3,11 +3,11 @@ import os
 import glob
 
 if __name__ == "__main__":
-    with open('../../../../wiki/ukbb/icdinfo/icdinfo.txt', 'r') as icd:
-        icdinfo = {line.split()[0]:line.split()[2] for line in icd}
-    hc_paths  = glob.glob('/oak/stanford/groups/mrivas/ukbb24983/phenotypedata/extras/highconfidenceqc/phe/*.phe')
-    hc_names  = list(map(lambda hc: icdinfo[hc] if hc in icdinfo else '',
-                         map(lambda path: os.path.splitext(os.path.basename(path))[0], hc_paths)))
+    hc_phenos  = glob.glob('/oak/stanford/groups/mrivas/ukbb24983/phenotypedata/extras/highconfidenceqc/phe/*.phe')
+    with open('highconfidenceqc_gbe_map.tsv', 'r') as map_file:
+        gbe_id_to_name = {line.split()[0]:line.split()[1] for line in map_file}
+    gbe_ids = [os.path.splitext(os.path.basename(path))[0] for path in hc_phenos]
+    hc_names = [gbe_id_to_name[gbe_id] for gbe_id in gbe_ids]
     make_phe_info(in_phe   = hc_paths,
                   out_path = '/oak/stanford/groups/mrivas/ukbb24983/phenotypedata/extras/highconfidenceqc/info/',
                   name     = hc_names,
