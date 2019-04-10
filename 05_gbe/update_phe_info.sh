ee17c72ce151a8d105118aebc128a26f8d32f4ba
Guhan Venkataraman <guhan@stanford.edu>
2019-04-10 15:25:51 -0700

    Update update_phe_info.sh

--- 05_gbe/update_phe_info.sh
+++ 05_gbe/update_phe_info.sh
@@ -10,7 +10,4 @@ OUT_PATH=$2
 LANG=C
 
 # finds all the info files, ensures formatting, concatenates them to desired output
-find $PHE_DIR -name "*.info" | xargs -i cat {} | awk -F'\t' 'NF == 14' | sort -k1,1 -k7,7nr | awk '!_[$1]++'  > ${OUT_PATH}/phenotype_info.tsv
-
-# also updates icdinfo
-awk -F'\t' 'BEGIN{OFS="\t"}(length($1) && length($2) && length($8)){print $1,$8,$2,$8,$8,"Y"}' ${OUT_PATH}/phenotype_info.tsv | tr " " "_" | sort -k1,1 -k2,2nr | awk '!_[$1]++' > ${OUT_PATH}/icdinfo.txt
+find ${PHE_DIR} -name "*.info" | awk -F'\t' 'NF == 14' | sort -k1,1 -k4,4nr | awk '!_[$1]++'  > ${OUT_PATH}/phenotype_info.tsv
