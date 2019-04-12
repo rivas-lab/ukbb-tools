88d94207eba92de41346ac9bf9004aee0bdf58f9
Matthew Aguirre <magu@sh-ln02.stanford.edu>
2019-04-12 10:03:36 -0700

    new data, legacy pandas versioning update and removed old code

--- 02_phenotyping/scripts/showcase_and_list_to_tsv.py
+++ 02_phenotyping/scripts/showcase_and_list_to_tsv.py
@@ -20,7 +20,7 @@ def join_and_add_cols(field_list, ref='../Data_Dictionary_Showcase.csv'):
     add_colnames = ['Annotator', 'Annotation date', 'Name', 'GBE ID', 'TableID', 'Field',
         'QT_total_num', 'BIN_total_num', 'QT_index', 'BIN_index', 'coding_exclude', 'coding_QT',
         'coding_binary_case', 'coding_binary_control']
-    subsetted = pd.concat([subsetted,pd.DataFrame(columns=add_colnames)], sort=True)
+    subsetted = pd.concat([subsetted,pd.DataFrame(columns=add_colnames)]).sort_values('FieldID')
 
     #Reorder the columns
     new_col_order = ['Annotator', 'Annotation date', 'Name', 'GBE ID', 'TableID', 'Field', 'FieldID',
