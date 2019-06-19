57bbdd48a0fc95fc3544bf8501513e441e932e06
David Amar <davidama@sh-102-07.int>
2019-06-19 10:39:49 -0700

    bugfix: wrong field name when using args.mem

--- 04_gwas/gwas.py
+++ 04_gwas/gwas.py
@@ -227,7 +227,7 @@ if __name__ == "__main__":
         if args.cores[0] is None:
             args.cores[0] = "4"
         if args.mem[0] is None:
-            args.mem[0] == "24000"
+            args.mem[0] = "24000"
     # lol i hope this works
     for flag,kind in filter(lambda x:x[0], zip(flags,kinds)):
         run_gwas(kind=kind, pheFile=args.pheno[0], outDir=args.outDir[0], 
