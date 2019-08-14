d60bb9c057c1f1fc57e9036eb14b46cc60ab10cc
Matthew Aguirre <magu@sh-ln06.stanford.edu>
2019-08-14 16:02:09 -0700

    added HLA GWAS

---
+++ 04_gwas/run_hla_gwas.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+#SBATCH --job-name=RL_HLA
+#SBATCH --output=rerun_logs/run_hla.%A_%a.out
+#SBATCH  --error=rerun_logs/run_hla.%A_%a.err
+#SBATCH --nodes=1
+#SBATCH --cores=1
+#SBATCH --mem=4000
+#SBATCH --time=2-00:00:00
+#SBATCH -p normal,owners
+
+set -beEuo pipefail
+
+# define functions
+usage () {
+    echo "$0: GWAS re-run script for the exome genotype data"
+    echo "usage: sbatch --array=1-<number of array jobs> $0 n_jobs (inclusive.)"
+    echo ''
+    echo '  You may check the status of the job (which jobs are finished) using the array-job module:'
+    echo '  $ ml load array-job'
+    echo '  $ array-job-find_ok.sh rerun_logs'
+}
+
+software_versions () {
+    which plink2
+    which bgzip
+}
+
+# get core and memory settings from the header -- passed to gwas script below
+cores=$(              cat $0 | egrep '^#SBATCH --cores='  | awk -v FS='=' '{print $NF}' )
+mem=$(                cat $0 | egrep '^#SBATCH --mem='    | awk -v FS='=' '{print $NF}' )
+log_dir=$( dirname $( cat $0 | egrep '^#SBATCH --output=' | awk -v FS='=' '{print $NF}' ))
+
+# check number of command line args and dump usage if that's not right
+if [ $# -lt 1 ] ; then usage >&2 ; exit 1 ; fi
+
+# load sofware, dump which versions are used
+export MODULEPATH="/home/groups/mrivas/.modules:${MODULEPATH}"
+ml load htslib
+
+if grep -q "CPU_GEN:HSW\|CPU_GEN:BDW\|CPU_GEN:SKX" <(a=$(hostname); sinfo -N -n ${a::-4} --format "%50f"); then
+   # AVX2 is suitable for use on this node if CPU is recent enough
+   ml load plink2/20190402
+else
+   ml load plink2/20190402-non-AVX2
+fi
+
+software_versions >&2
+
+# job start header (for use with array-job module)
+_SLURM_JOBID=${SLURM_JOBID:=0} # use 0 for default value (for debugging purpose)
+_SLURM_ARRAY_TASK_ID=${SLURM_ARRAY_TASK_ID:=1}
+echo "[$0 $(date +%Y%m%d-%H%M%S)] [array-start] hostname = $(hostname) SLURM_JOBID = ${_SLURM_JOBID}; SLURM_ARRAY_TASK_ID = ${_SLURM_ARRAY_TASK_ID}" >&2
+
+# get phenotypes to run
+n_jobs=$1
+this_idx=$_SLURM_ARRAY_TASK_ID
+min_N_count=10
+
+# iterate and run
+for phe_path in $(cat ../05_gbe/phenotype_info.tsv | awk -v min_N=${min_N_count} 'NR > 1 && $7 >= min_N' | awk -v idx=$this_idx -v mod=$n_jobs '((NR % mod)==idx) {print $NF}' ); do 
+    gbeId=$(basename $phe_path | awk '{gsub(".phe","");print}')
+
+    # run gwas with default GBE parameters
+    pop="white_british"
+    gwasOutDir=$(echo $(dirname $(dirname $phe_path)) | awk '{gsub("phenotypedata","hla/gwas"); print}')/${pop}
+    if [ ! -d ${gwasOutDir}/logs ] ; then mkdir -p ${gwasOutDir}/logs ; fi
+    if [ ! -d $log_dir ] ; then mkdir -p $log_dir ; fi
+
+    python gwas.py --run-hla --run-now --memory $mem --cores $cores --pheno $phe_path --out $gwasOutDir --population $pop --log-dir $log_dir
+
+    # move log file and bgzip output - NEEDS TO BE LOOKED AT CLOSER/TESTED
+    for type in exome-spb; do 
+        file_prefix=${gwasOutDir}/ukb24983_v2_hg38.${gbeId}.${type}
+        for ending in "logistic.hybrid" "linear"; do
+        if [ -f ${file_prefix}.PHENO1.glm.${ending} ]; then
+            bgzip --compress-level 9 -f ${file_prefix}.PHENO1.glm.${ending}
+        fi
+        done
+        if [ -f ${file_prefix}.log ]; then
+        mv -f ${file_prefix}.log ${gwasOutDir}/logs/
+        fi    
+    done
+done
+
+# job finish footer (for use with array-job module)
+echo "[$0 $(date +%Y%m%d-%H%M%S)] [array-end] hostname = $(hostname) SLURM_JOBID = ${_SLURM_JOBID}; SLURM_ARRAY_TASK_ID = ${_SLURM_ARRAY_TASK_ID}" >&2
