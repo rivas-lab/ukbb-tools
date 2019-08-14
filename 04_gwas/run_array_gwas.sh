43c7faa37d55ec33f6957c5e8f70b4b2319f7c4c
Yosuke Tanigawa <ytanigaw@stanford.edu>
2019-08-14 16:55:03 -0700

    update run_array_gwas.sh so that it supports multiple populations

--- 04_gwas/run_array_gwas.sh
+++ 04_gwas/run_array_gwas.sh
@@ -25,6 +25,13 @@ software_versions () {
     which bgzip
 }
 
+get_field_from_pop () {
+    local pop=$1
+    local const=7
+    echo "white_british non_british_white african e_asian s_asian" \
+        | tr " " "\n" | awk -v pop=$pop -v const=$const '($0 == pop){print NR + const}'
+}
+
 # get core and memory settings from the header -- passed to gwas script below
 cores=$(              cat $0 | egrep '^#SBATCH --cores='  | awk -v FS='=' '{print $NF}' )
 mem=$(                cat $0 | egrep '^#SBATCH --mem='    | awk -v FS='=' '{print $NF}' )
@@ -32,6 +39,13 @@ log_dir=$( dirname $( cat $0 | egrep '^#SBATCH --output=' | awk -v FS='=' '{prin
 
 # check number of command line args and dump usage if that's not right
 if [ $# -lt 1 ] ; then usage >&2 ; exit 1 ; fi
+
+# population
+if [ $# -gt 1 ] ; then pop=$2 ; else pop="white_british" ; fi
+if [ ${pop} != "white_british" ] && [ ${pop} != "non_british_white" ] && [ ${pop} != "african" ] && [ ${pop} != "e_asian" ] && [ ${pop} != "s_asian" ] ; then
+    echo "unsupported population: ${pop}" >&2 ; exit 1 
+fi
+field=$(get_field_from_pop $pop)
 
 # load sofware, dump which versions are used
 export MODULEPATH="/home/groups/mrivas/.modules:${MODULEPATH}"
@@ -49,19 +63,13 @@ software_versions >&2
 # job start header (for use with array-job module)
 _SLURM_JOBID=${SLURM_JOBID:=0} # use 0 for default value (for debugging purpose)
 _SLURM_ARRAY_TASK_ID=${SLURM_ARRAY_TASK_ID:=1}
-echo "[$0 $(date +%Y%m%d-%H%M%S)] [array-start] hostname = $(hostname) SLURM_JOBID = ${_SLURM_JOBID}; SLURM_ARRAY_TASK_ID = ${_SLURM_ARRAY_TASK_ID}" >&2
+echo "[$0 $(date +%Y%m%d-%H%M%S)] [array-start] hostname = $(hostname) SLURM_JOBID = ${_SLURM_JOBID}; SLURM_ARRAY_TASK_ID = ${_SLURM_ARRAY_TASK_ID} ; pop=${pop}" >&2
 
 # get phenotypes to run
 start_idx=$1
 this_idx=$_SLURM_ARRAY_TASK_ID
 
 min_N_count=10
-
-# Fill this in with which ones you want. 
-# white_british=8, non_british_white=9, african=10, e_asian=11, s_asian=12
-
-pop="non_british_white"
-field=9
 
 phe_path=$(cat ../05_gbe/phenotype_info.tsv | awk -v min_N=${min_N_count} -v col="$field" 'NR > 1 && $col >= min_N' | egrep -v MED | grep BIN10030500 | awk -v start_idx=$start_idx -v this_idx=$this_idx 'NR==(start_idx + this_idx - 1) {print $NF}' )
 gbeId=$(basename $phe_path | awk '{gsub(".phe","");print}')
@@ -86,6 +94,6 @@ for type in genotyped; do
     fi
 done
 
-
 # job finish footer (for use with array-job module)
-echo "[$0 $(date +%Y%m%d-%H%M%S)] [array-end] hostname = $(hostname) SLURM_JOBID = ${_SLURM_JOBID}; SLURM_ARRAY_TASK_ID = ${_SLURM_ARRAY_TASK_ID}" >&2
+echo "[$0 $(date +%Y%m%d-%H%M%S)] [array-end] hostname = $(hostname) SLURM_JOBID = ${_SLURM_JOBID}; SLURM_ARRAY_TASK_ID = ${_SLURM_ARRAY_TASK_ID} ; pop=${pop}" >&2
+
