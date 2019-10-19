9ab85688b00b4c11ce5ecb876d7b47f70d8ccb59
Yosuke Tanigawa <ytanigaw@stanford.edu>
2019-10-19 09:29:07 -0700

    update LD map scripts for imputation dataset

--- 14_LD_map/imp_v3_ldmap.sbatch.sh
+++ 14_LD_map/imp_v3_ldmap.sbatch.sh
@@ -3,16 +3,18 @@
 #SBATCH --output=imp_v3_ldmap_logs/ldmap.%A_%a.out
 #SBATCH  --error=imp_v3_ldmap_logs/ldmap.%A_%a.err
 #SBATCH --nodes=1
-#SBATCH --cores=8
+#SBATCH --cores=6
 #SBATCH --mem=51200
 #SBATCH --time=2-00:00:00
-#SBATCH -p mrivas
+#SBATCH -p mrivas,normal
 
 set -beEuo pipefail
 
-pop="white_british"
-pop="e_asian"
-out_dir="/scratch/groups/mrivas/ukbb/24983/imp/ldmap"
+#########################
+pop=$1
+#pop="white_british"
+#pop="e_asian"
+#########################
 
 array_job_idx_to_chrom () {
     local idx=$1
@@ -32,12 +34,12 @@ array_job_idx_to_chrom () {
 compute_ld_map () {
     local chrom=$1
     local pop=$2
-    local out_dir=$3
-
+    
+    local out_dir="/scratch/groups/mrivas/ukbb/24983/imp/ldmap"
     local ukbb_dir="$OAK/ukbb24983"
     local keep_file="${ukbb_dir}/sqc/population_stratification/ukb24983_${pop}.phe"
-    local bfile="${ukbb_dir}/imp/pgen/ukb24983_imp_chr${chrom}_v3"
-    local out_prefix="${out_dir}/ukb24983_imp_chr${chrom}_v3.${pop}"
+    local bfile="${ukbb_dir}/imp/pgen/var_QC/ukb24983_imp_chr${chrom}_v3_QC"
+    local out_prefix="${out_dir}/ukb24983_imp_chr${chrom}_v3_QC.${pop}"
 
     ml load plink
 
@@ -59,9 +61,8 @@ echo "[$0 $(date +%Y%m%d-%H%M%S)] [array-start] hostname = $(hostname) ; pop = $
 
 #########################
 chrom=$(array_job_idx_to_chrom ${_SLURM_ARRAY_TASK_ID})
-compute_ld_map ${chrom} ${pop} ${out_dir}
+compute_ld_map ${chrom} ${pop}
 #########################
 
 # job finish footer (for use with array-job module)
 echo "[$0 $(date +%Y%m%d-%H%M%S)] [array-end] hostname = $(hostname) ; pop = ${pop} ; SLURM_JOBID = ${_SLURM_JOBID}; SLURM_ARRAY_TASK_ID = ${_SLURM_ARRAY_TASK_ID}" >&2
-
