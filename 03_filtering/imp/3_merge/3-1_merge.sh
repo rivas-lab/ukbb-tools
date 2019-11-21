ee9d6ed5c1a8ffde50f060883693073f4a93661e
Yosuke Tanigawa <ytanigaw@stanford.edu>
2019-11-20 16:02:09 -0800

    add scripts for GCTA GRM

--- 03_filtering/imp/3_merge/3-1_merge.sh
+++ 03_filtering/imp/3_merge/3-1_merge.sh
@@ -20,33 +20,7 @@ echo "tmp_dir = $tmp_dir" >&2
 handler_exit () { rm -rf $tmp_dir ; }
 trap handler_exit EXIT
 ##############
-
-cat_or_zstdcat () {
-    local file=$1
-    if [ "${file%.zst}.zst" == "${file}" ] ; then zstdcat $file ; else cat $file ; fi
-}
-
-prep_files () {
-    local zstfile=$1
-    local prefix=$2
-    local tmp_dir=$3
-    
-    local pfile=$(echo $zstfile | sed -e "s/.zst$//g" | sed -e "s/.pvar$//g")
-    local tmp_pvar="${tmp_dir}/${prefix}.tmp.in.pvar"
-    local tmp_bim="${tmp_dir}/$(basename $pfile).bim"
-    local tmp_bed="${tmp_dir}/$(basename $pfile).bed"
-    local tmp_fam="${tmp_dir}/$(basename $pfile).fam"
-
-    ln -s ${pfile}.fam ${tmp_fam}    
-    ln -s ${pfile}.bed ${tmp_bed}
-    
-    cat_or_zstdcat ${zstfile} | grep -v '##' > ${tmp_pvar}
-    Rscript assign_short_names_for_merge.R ${tmp_pvar} ${prefix} ${pfile}.IDs.tsv
-    rm ${tmp_pvar}
-    
-    cat ${pfile}.IDs.tsv \
-    | awk -v FS='\t' -v OFS='\t' '(NR>1){print $1, $6, 0, $2, $5, $4}' > ${tmp_bim}
-}
+source $(dirname $(readlink -f $0))/3-0_merge_misc.sh
 ##############
 
 merge_list_tsv=$1
@@ -57,8 +31,8 @@ tmp_merge_list=${tmp_dir}/merge.lst
 
 # prepare temp files for merge (assign short IDs)
 cat ${merge_list_tsv} | while read f p ; do 
-    echo "processing $f" >&2
-    prep_files $f $p $tmp_dir
+    echo "processing $f" >&2    
+    prep_files $f $p $tmp_dir $(dirname $(readlink -f $0))/assign_short_names_for_merge.R
     echo ${tmp_dir}/$(basename $f) | sed -e "s/.zst$//g" | sed -e "s/.pvar$//g" >> ${tmp_merge_list}
 done
 
