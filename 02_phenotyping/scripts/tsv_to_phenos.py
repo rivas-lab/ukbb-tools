013b02d82e20468cf64b2dfbc43fc108fc6e7dad
Guhan Ram Venkataraman <guhan@sh-ln08.stanford.edu>
2020-01-31 10:49:02 -0800

    added README for annotation

--- 02_phenotyping/scripts/tsv_to_phenos.py
+++ 02_phenotyping/scripts/tsv_to_phenos.py
@@ -9,11 +9,13 @@ This script is designed to process table-defined phenotypes (from a Rivas lab co
 
 To define all phenotypes from a table, identify the (zero-indexed!) columns specified below, and pass them to the script. If you would like to define only one phenotype from the table, you can do that by adding the flag --only-this-row, followed by the row (zero-indexed, such that the first non-header row is number 0) you'd like to run.
 
-Author: Matthew Aguirre (SUNET: magu)
+Author: Matthew Aguirre and Guhan Venkataraman (SUNETs: magu and guhan)
+Updated: 2020/01/29
 """
 
-def get_phe_definitions(in_tsv, table_col, field_col, name_col, case_col, ctrl_col,
-                  excl_col, qtfc_col, desc_col, header=True, make_this_one=None):
+def get_phe_definitions(in_tsv, field_col, name_col, case_col, ctrl_col, excl_col, qtfc_col, 
+             desc_col, ann_col, ann_d_col, ftbd, header=True, make_this_one=None):
+
     # get phenotype definitions from an input table, with option to only extract one row
     phe_info = {}
     if make_this_one is not None:
@@ -22,7 +24,7 @@ def get_phe_definitions(in_tsv, table_col, field_col, name_col, case_col, ctrl_c
         else:
             make_this_one = int(make_this_one)
     with open(in_tsv, 'r') as f:
-        for i,line in enumerate(f):
+        for i, line in enumerate(f):
             if header and i==0:
                 if make_this_one is not None:
                     make_this_one += 1
@@ -30,64 +32,67 @@ def get_phe_definitions(in_tsv, table_col, field_col, name_col, case_col, ctrl_c
             if make_this_one is not None and i != make_this_one:
                 continue
             fields = line.rstrip().split('\t')
-            if not fields[name_col]: continue
+            if not ((fields[name_col] and fields[desc_col] and fields[field_col]) and (fields[case_col] or fields[ctrl_col] or fields[excl_col] or fields[qtfc_col] or fields[ann_col] or fields[ann_d_col])): 
+                if fields[name_col]:
+                    print("No annotation data present for " + fields[name_col] + ".")
+                continue
+            field_metadata = ftbd[ftbd['Field_ID'] == fields[field_col]]
             # the if switch is in case empty cells get pruned from the TSV
-            phe_info[ fields[name_col] ] = {'case':     fields[case_col] if case_col < len(fields) else '',
-                                            'control':  fields[ctrl_col] if ctrl_col < len(fields) else '',
-                                            'qt_order': fields[qtfc_col] if qtfc_col < len(fields) else '',
-                                            'exclude':  fields[excl_col] if excl_col < len(fields) else '',
-                                            'table_id': fields[table_col] if table_col < len(fields) else '',
-                                            'field_id': fields[field_col] if field_col < len(fields) else '',
-                                            'desc':     fields[desc_col] if desc_col < len(fields) else ''}
+            phe_info[ fields[name_col] ] = {'case':      fields[case_col] if case_col < len(fields) else '',
+                                            'control':   fields[ctrl_col] if ctrl_col < len(fields) else '',
+                                            'qt_order':  fields[qtfc_col] if qtfc_col < len(fields) else '',
+                                            'exclude':   fields[excl_col] if excl_col < len(fields) else '',
+                                            'table_id':  field_metadata['Table_ID'].values[0] if len(field_metadata) > 0 else '',
+                                            'basket_id': field_metadata['Basket_ID'].values[0] if len(field_metadata) > 0 else '',
+                                            'field_id':  fields[field_col] if field_col < len(fields) else '',
+                                            'desc':      fields[desc_col] if desc_col < len(fields) else ''}
     return phe_info
 
 
-def define_phenos(in_tsv, table_col, field_col, name_col, case_col, ctrl_col, 
-                  excl_col, qtfc_col, desc_col, new_table=True, table_id=None, 
+def define_phenos(in_tsv, field_col, name_col, case_col, ctrl_col, 
+                  excl_col, qtfc_col, desc_col, ann_col, ann_d_col, ftbd,
                   header=True, all_ctrl=False, make_this_one=None):
     home_dir='/oak/stanford/groups/mrivas/private_data/ukbb/24983/phenotypedata'
     # iterate over phenotypes in the input file
-    for phe_name, phe_values in get_phe_definitions(in_tsv, table_col, field_col, name_col, 
-                                                    case_col, ctrl_col, excl_col, qtfc_col, 
-                                                    desc_col, header, make_this_one).items():
-        print(phe_name, phe_values)
-        # select tab file to use from handler arguments TODOTODOTODOTODO wrong
-        if new_table:
-            tsv = map(glob.glob, [os.path.join(root,'current/ukb*.tab') for root,dirs,files in os.walk(home_dir) if phe_values['table_id'] in dirs])[0][0]
-            table_id = os.path.splitext(os.path.basename(tsv))[0].replace('ukb','')
-        else:
-            table_id = phe_values['table_id'] if table_id is None else table_id
-            tab_f = 'ukb{}.tab'.format(table_id)
-            # this will throw an indexing error if a bad table is supplied
-            tsv = [os.path.join(root,tab_f) for root,dirs,files in os.walk(home_dir) if tab_f in files][0]
-        # get phenotype name
-        basket_id = os.path.basename(os.path.dirname(os.path.dirname(os.path.dirname(tsv))))
-        phe = os.path.join(home_dir, basket_id, table_id, 'phe', phe_name+'.phe')
-        print(tsv, phe)
+    for phe_name, phe_values in get_phe_definitions(in_tsv, field_col, name_col, case_col, ctrl_col, excl_col, 
+                qtfc_col, desc_col, ann_col, ann_d_col, ftbd, header, make_this_one).items():
+        phe = os.path.join(home_dir, phe_values['basket_id'], phe_values['table_id'], 'phe', phe_name + '.phe')
         log = os.path.join(os.path.dirname(os.path.dirname(phe)), "logs/{0}.log".format(phe_name))
-        # assume binary if we have a case definition, else assume qt
-        if phe_values['case']: 
-            # this and create_qt_phe_file below are implemented in make_phe.py
-            create_bin_phe_file(in_tsv = tsv, out_phe = phe, out_log = log, field_id = phe_values['field_id'],
-                                case     = phe_values['case'].replace(',',';').split(';'),
-                                control  = phe_values['control'].replace(',',';').split(';'),
-                                missing_is_control = all_ctrl if phe_values['control'] else True)
-        else: 
-            create_qt_phe_file(in_tsv = tsv, out_phe = phe, out_log = log, field_id = phe_values['field_id'],
-                               order    = phe_values['qt_order'].replace(',',';').split(';'),
-                               exclude  = phe_values['exclude'].replace(',',';').split(';'))
-        # annotate the phenotype
-        make_phe_info([phe], 
-                       os.path.join(os.path.dirname(os.path.dirname(phe)), "info"), 
-                      [phe_values['desc']],
-                       phe_values['field_id'],
-                       phe_values['table_id'],
-                       basket_id,
-                       '24983', 
-                       os.path.basename(in_tsv))
-                       
+        info_dir = os.path.join(os.path.dirname(os.path.dirname(phe)), "info")
+        info_file = os.path.join(info_dir, "{}.info".format(phe_name))
+        #if os.path.exists(phe) and os.path.exists(info_file):
+        #    print("Phenotype for " + phe_name + " already exists at " + phe + ".")
+        #else:
+        if True:
+            tab_f = 'ukb{}.tab'.format(phe_values['table_id'] )
+            # this will throw an indexing error if a bad table is supplied
+            tsv = os.path.join(home_dir, phe_values['basket_id'], phe_values['table_id'], 'download', tab_f)
+            # assume binary if we have a case definition, else assume qt
+            if phe_values['case']: 
+                # this and create_qt_phe_file below are implemented in make_phe.py
+                create_bin_phe_file(in_tsv = tsv, out_phe = phe, out_log = log, field_id = phe_values['field_id'],
+                                    case     = phe_values['case'].replace(',',';').split(';'),
+                                    control  = phe_values['control'].replace(',',';').split(';'),
+                                    missing_is_control = all_ctrl if phe_values['control'] else True)
+            else:
+                create_qt_phe_file(in_tsv = tsv, out_phe = phe, out_log = log, field_id = phe_values['field_id'],
+                                   order    = phe_values['qt_order'].replace(',',';').split(';'),
+                                   exclude  = phe_values['exclude'].replace(',',';').split(';'))
+            # annotate the phenotype
+            make_phe_info([phe], 
+                           info_dir, 
+                          [phe_values['desc']],
+                           phe_values['field_id'],
+                           phe_values['table_id'],
+                           phe_values['basket_id'],
+                           '24983', 
+                           os.path.basename(in_tsv))
+            if os.path.exists(os.path.join(home_dir, "current", "phe/{}.phe".format(phe_name))):
+                for folder, filetype in zip(["phe", "info", "logs"], ["phe", "info", "log"]):
+                    os.remove(os.path.join(home_dir, "current", folder, phe_name + "." + filetype))
+            for path, folder, filetype in zip([phe, info_file, log], ["phe", "info", "logs"], ["phe", "info", "log"]):
+                os.symlink(path, os.path.join(home_dir, "current", folder, phe_name + "." + filetype))
     return
-
 
 if __name__=="__main__":
     import argparse
@@ -105,12 +110,6 @@ if __name__=="__main__":
                             help='column in tsv corresponding to phenotype description (GBE NAME string)')
     parser.add_argument('--field', dest="field", required=True, nargs=1,
                             help='column in tsv corresponding to UK Biobank Field ID')
-    parser.add_argument('--table', dest="table", required=True, nargs=1,
-                            help='column in tsv corresponding to UK Biobank Table ID')
-    parser.add_argument('--table-id', dest="table_id", required=False, nargs=1, default=[None],
-                            help='ID of UK Biobank Table ID to use (overrides table option)')
-    parser.add_argument('--table-newest', dest="new_table", action='store_true',
-                            help='Flag to use newest table corresponding to UK Biobank basket (overrides table and table-id options)')
     parser.add_argument('--case', dest='case', required=True, nargs=1,
                             help='column in tsv corresponding to values for binary case definitions')
     parser.add_argument('--control', dest='control', required=True, nargs=1,
@@ -121,13 +120,18 @@ if __name__=="__main__":
                             help='column in tsv corresponding to QT values considered as missing data')
     parser.add_argument('--order', dest='order', required=True, nargs=1,
                             help='column in tsv corresponding to order of values (least to greatest) for QTs from categorical fields')
+    parser.add_argument('--annotator', dest='annotator', required=True, nargs=1,
+                            help='column in tsv corresponding to the annotator name')
+    parser.add_argument('--annotator_date', dest='annotator_date', required=True, nargs=1,
+                            help='column in tsv corresponding to the annotation date')
     parser.add_argument('--only-this-row', dest='onlyone', required=False, nargs=1,
                             help='(optional) flag to run only one (zero-indexed, not including the header) row the input tsv.') 
     args = parser.parse_args()
-    print(args)
-    # lol i hope this works
-    define_phenos(in_tsv    = args.input[0],
-           table_col = int(args.table[0]),
+    # File gives the mapping between table, field, basket, and release date 
+    ftbd = pd.read_csv('/oak/stanford/groups/mrivas/users/guhan/repos/ukbb-tools/05_gbe/field_table_basket_date.tsv', sep='\t', dtype=object)
+    # Retain only the latest release per field
+    ftbd = ftbd.sort_values('Release_Date').groupby('Field_ID').first().reset_index()
+    define_phenos(in_tsv = args.input[0],
            field_col = int(args.field[0]),
            name_col  = int(args.name[0]),
            case_col  = int(args.case[0]),
@@ -135,8 +139,9 @@ if __name__=="__main__":
            excl_col  = int(args.exclude[0]),
            qtfc_col  = int(args.order[0]),
            desc_col  = int(args.desc[0]),
-           new_table = args.new_table,
-           table_id  = args.table_id[0],
+           ann_col   = int(args.annotator[0]),
+           ann_d_col = int(args.annotator_date[0]),
+           ftbd      = ftbd,
            header    = not args.noheader,
            all_ctrl  = args.expand_control,
            make_this_one = args.onlyone)
--- 04_gwas/extras/bbj/cleanup_bbj.sh
+++ 04_gwas/extras/bbj/cleanup_bbj.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-mv /oak/stanford/groups/mrivas/bbj/bbj_variant_annots.tsv.bgz /oak/stanford/groups/mrivas/bbj/bbj_variant_annots.tsv.gz
+mv /oak/stanford/groups/mrivas/bbj/variant_annotation/bbj_variant_annots.tsv.bgz /oak/stanford/groups/mrivas/bbj/variant_annotation/bbj_variant_annots.tsv.gz
 
-zcat /oak/stanford/groups/mrivas/bbj/bbj_variant_annots.tsv.gz | cut -f3- >/oak/stanford/groups/mrivas/bbj/bbj_variant_annots.tsv
+zcat /oak/stanford/groups/mrivas/bbj/variant_annotation/bbj_variant_annots.tsv.gz | cut -f3- >/oak/stanford/groups/mrivas/bbj/variant_annotation/bbj_variant_annots.tsv
 
 #remove all the nulls
-sed -i 's/null,//g' /oak/stanford/groups/mrivas/bbj/bbj_variant_annots.tsv
-sed -i 's/,null//g' /oak/stanford/groups/mrivas/bbj/bbj_variant_annots.tsv
+sed -i 's/null,//g' /oak/stanford/groups/mrivas/bbj/variant_annotation/bbj_variant_annots.tsv
+sed -i 's/,null//g' /oak/stanford/groups/mrivas/bbj/variant_annotation/bbj_variant_annots.tsv
 #replace with NAs
-sed -i 's/\[null\]/NA/g' /oak/stanford/groups/mrivas/bbj/bbj_variant_annots.tsv
+sed -i 's/\[null\]/NA/g' /oak/stanford/groups/mrivas/bbj/variant_annotation/bbj_variant_annots.tsv
 #get rid of quotes
-sed -i 's/"//g' /oak/stanford/groups/mrivas/bbj/bbj_variant_annots.tsv
+sed -i 's/"//g' /oak/stanford/groups/mrivas/bbj/variant_annotation/bbj_variant_annots.tsv
 #replace NAs with quotes
-sed -i 's/\<NA\>/""/g' /oak/stanford/groups/mrivas/bbj/bbj_variant_annots.tsv
+sed -i 's/\<NA\>/""/g' /oak/stanford/groups/mrivas/bbj/variant_annotation/bbj_variant_annots.tsv
 #remove brackets
-sed -i 's/\[//g' /oak/stanford/groups/mrivas/bbj/bbj_variant_annots.tsv
-sed -i 's/\]//g' /oak/stanford/groups/mrivas/bbj/bbj_variant_annots.tsv
-awk -F'\t' 'BEGIN{OFS="\t"}{if (NR == 1) { print $0 } else {print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9"\t"$10"\t"$11"\t"$12"\tCSQ="$13"\t"$14"\t"$15"\t"$16"\t"$17"\t"$18"\t"$19"\t"$20"\t"$21"\t"$22"\t"$23"\t"$24"\t"$25"\t"$26"\t"$27$"\t"$28"\t"$29"\t"$30}}' /oak/stanford/groups/mrivas/bbj/bbj_variant_annots.tsv >bbj_variant_annots_tmp.tsv && mv bbj_variant_annots_tmp.tsv /oak/stanford/groups/mrivas/bbj/bbj_variant_annots.tsv
+sed -i 's/\[//g' /oak/stanford/groups/mrivas/bbj/variant_annotation/bbj_variant_annots.tsv
+sed -i 's/\]//g' /oak/stanford/groups/mrivas/bbj/variant_annotation/bbj_variant_annots.tsv
 
-gzip -f /oak/stanford/groups/mrivas/bbj/bbj_variant_annots.tsv
+awk -F'\t' 'BEGIN{OFS="\t"}{if (NR == 1) { print $0 } else { $13="CSQ="$13; print}}' /oak/stanford/groups/mrivas/bbj/variant_annotation/bbj_variant_annots.tsv >bbj_variant_annots_tmp.tsv && mv bbj_variant_annots_tmp.tsv /oak/stanford/groups/mrivas/bbj/variant_annotation/bbj_variant_annots_tmp.tsv
+
+#gzip -f /oak/stanford/groups/mrivas/bbj/variant_annotation/bbj_variant_annots.tsv
