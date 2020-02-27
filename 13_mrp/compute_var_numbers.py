04f2d0df302c897de016714a7d7ec8c4bb38b684
Guhan Ram Venkataraman <guhan@sh02-01n58.int>
2020-02-27 15:41:28 -0800

    added MPC suppor

--- 13_mrp/compute_var_numbers.py
+++ 13_mrp/compute_var_numbers.py
@@ -60,7 +60,7 @@ def build_R_phen_row(pheno1, phen_info, row_num):
     """
     # Filter for SE as you read it in
     print("Reading in metadata...")
-    metadata = pd.read_table('/oak/stanford/groups/mrivas/ukbb24983/cal/pgen/ukb_cal-consequence_wb_maf_gene_ld_indep.tsv')
+    metadata = pd.read_table('/oak/stanford/groups/mrivas/ukbb24983/cal/pgen/ukb_cal-consequence_wb_maf_gene_ld_indep_mpc_pli.tsv')
     phen1_path = list(phen_info[phen_info['PHEN'] == pheno1]['path'])[0]
     print("Filtering on p-value and SE...")
     df1 = set_up_df(phen1_path, metadata, pheno1)
