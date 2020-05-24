342923ae685496c630ab82e96d5219022477a976
Yosuke Tanigawa <ytanigaw@stanford.edu>
2020-05-23 20:23:26 -0700

    update the array + imp combined dataset (v2)

--- 03_filtering/imp/3_merge/3-3_bed_to_pgen_v2.sh
+++ 03_filtering/imp/3_merge/3-3_bed_to_pgen_v2.sh
@@ -3,10 +3,10 @@
 #SBATCH --output=logs/pgen.%A.out
 #SBATCH  --error=logs/pgen.%A.err
 #SBATCH --nodes=1
-#SBATCH --cores=6
-#SBATCH --mem=60000
+#SBATCH --cores=4
+#SBATCH --mem=40000
 #SBATCH --time=2-0:00:00
-#SBATCH -p mrivas,normal
+# SBATCH -p mrivas,normal
 set -beEuo pipefail
 
 cores=$( cat $0 | egrep '^#SBATCH --cores='  | awk -v FS='=' '{print $NF}' )
@@ -14,7 +14,8 @@ mem=$(   cat $0 | egrep '^#SBATCH --mem='    | awk -v FS='=' '{print $NF}' )
 
 out="/oak/stanford/groups/mrivas/ukbb/24983/array_imp_combined/pgen_v2/ukb24983_ukb24983_cal_hla_cnv_imp"
 
-plink_opts="--memory ${mem} --threads ${cores}"
+plink_mem=$( perl -e "print(int(${mem} * 0.8))" )
+plink_opts="--memory ${plink_mem} --threads ${cores}"
 plink2 ${plink_opts} --bfile ${out} --keep-allele-order --make-pgen vzs --out ${out}
 
 mv "${out}.log" "${out}.pgen.log"
--- 03_filtering/imp/4_split/README.md
+++ 03_filtering/imp/4_split/README.md
@@ -1,4 +1,7 @@
-### split
+### (obsolete) split
+
+This information is obsolete as of 2020/5/23 as snpnet now supports the in-memory split of train/val in pgen file.
 
 For PRS models, we split the `array_imp_combined` dataset into training/validation/test set.
 We use the random split from DeGAs-risk paper.
+
