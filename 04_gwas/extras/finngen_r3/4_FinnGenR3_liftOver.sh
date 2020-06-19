de2ebaeb0d8899fc6f0f085fac983aab511a0d44
Yosuke Tanigawa <ytanigaw@stanford.edu>
2020-06-18 22:48:55 -0700

    update liftOver script for FinnGen R3

--- 04_gwas/extras/finngen_r3/4_FinnGenR3_liftOver.sh
+++ 04_gwas/extras/finngen_r3/4_FinnGenR3_liftOver.sh
@@ -9,9 +9,10 @@ if [ ! -d $(dirname ${out_file}) ] ; then mkdir -p $(dirname ${out_file}) ; fi
 if [ ! -f ${out_file}.gz ] ; then
 
     Rscript 4_FinnGenR3_liftOver.R ${in_file} ${out_file}
-    bgzip -l9 -@2 ${out_file}
+    cat ${out_file} | sort --parallel 2 -k1,1V -k2,2n -k5,5 | bgzip -l9 -@4 > ${out_file}.gz
     tabix -c '#' -s 1 -b2 -e2 ${out_file}.gz
 
     echo ${out_file}.gz
 
 fi
+
