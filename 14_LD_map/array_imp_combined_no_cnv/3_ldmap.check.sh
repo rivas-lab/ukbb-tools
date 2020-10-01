a35aa86a88a2a2a6198e3bf7ff37b34d2119f5ac
Yosuke Tanigawa <ytanigaw@stanford.edu>
2020-10-01 00:01:16 -0700

    update LD map check script

--- 14_LD_map/array_imp_combined_no_cnv/3_ldmap.check.sh
+++ 14_LD_map/array_imp_combined_no_cnv/3_ldmap.check.sh
@@ -11,7 +11,14 @@ pops=(
 
 source /oak/stanford/groups/mrivas/users/ytanigaw/repos/rivas-lab/ukbb-tools/14_LD_map/14_LD_map_misc.sh
 
+version='20200928'
+oak_or_scratch='/scratch'
+oak_or_scratch='/oak/stanford'
+
+data_d="${oak_or_scratch}/groups/mrivas/ukbb24983/array_imp_combined_no_cnv/ldmap/ldmap_${version}"
+
 for pop in ${pops[@]} ; do
-    out_prefix="/scratch/groups/mrivas/ukbb24983/array_imp_combined_no_cnv/ldmap/ukb24983_cal_hla_imp.${pop}"
+    out_prefix="${data_d}/ukb24983_cal_hla_imp.${pop}"
     check_ldmap_out ${out_prefix}
 done
+
