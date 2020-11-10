1c30b4b71aa7a4f68e1ccb95e6eb3c36d0bad393
Yosuke Tanigawa <ytanigaw@stanford.edu>
2020-11-10 08:23:03 -0800

    update concat script (for larger files)

--- 04_gwas/extras/20201026_exome_gwas_parallel/10d_master_gwas_concat_chromosomes.sh
+++ 04_gwas/extras/20201026_exome_gwas_parallel/10d_master_gwas_concat_chromosomes.sh
@@ -7,16 +7,17 @@ data_d="/scratch/groups/mrivas/ukbb24983/exome/gwas/master_phe_20201002_exomeOQF
 out_f=${data_d}/ukb24983_exomeOQFE.${pop}.p1e-3.tsv.gz
 
 if [ ! -f ${out_f} ] ; then
-    {
-        ! zcat ${data_d}/ukb24983_exomeOQFE.${pop}.chr1.p1e-3.tsv.gz | head -n1
-
-        for chrom in $(seq 1 22) X Y ; do
-            zcat ${data_d}/ukb24983_exomeOQFE.${pop}.chr${chrom}.p1e-3.tsv.gz \
-            | egrep -v '^#'
-        done
-    } | bgzip -l9 -@6 > ${out_f}
+    ! zcat ${data_d}/ukb24983_exomeOQFE.${pop}.chr1.p1e-3.tsv.gz | head -n1 > ${out_f%.gz}
+    for chrom in $(seq 1 22) X Y ; do
+        echo "[$(date +%Y%m%d-%H%M%S)] chr${chrom}" >&2
+        pigz -dc ${data_d}/ukb24983_exomeOQFE.${pop}.chr${chrom}.p1e-3.tsv.gz | egrep -v '^#' >> ${out_f%.gz}
+    done
+    echo "[$(date +%Y%m%d-%H%M%S)] bgzip" >&2
+    bgzip -f -l9 -@6 ${out_f%.gz}
 fi
 
 if [ ! -f ${out_f}.tbi ] ; then
+    echo "[$(date +%Y%m%d-%H%M%S)] tabix" >&2
     tabix -s1 -b2 -e2 -c'#' ${out_f}
 fi
+
