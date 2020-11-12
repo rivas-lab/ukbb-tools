bb24d83d6770171f09eff83bcff9dcb7eba2577d
Yosuke Tanigawa <ytanigaw@stanford.edu>
2020-11-12 08:36:22 -0800

    add rclone script

---
+++ 04_gwas/extras/20201026_exome_gwas_parallel/12b_rclone.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -beEuo pipefail
+
+pop=$1
+
+ml load rclone
+
+echo "[$(date +%Y%m%d-%H%M%S)] ${pop} start"
+rclone copy \
+/scratch/groups/mrivas/ukbb24983/exome/gwas/freeze/master_phe_20201002_exomeOQFE_20201110/master_phe_20201002_exomeOQFE_20201110.${pop}.tar \
+gdrive://rivas-lab/ukbb24983/exome/gwas/freeze/master_phe_20201002_exomeOQFE_20201110
+echo "[$(date +%Y%m%d-%H%M%S)] ${pop} done"
+
+exit 0
+##############################
+
+sbatch -p mrivas --qos=high_p \
+--nodes=1 --mem=6000 --cores=1 --time=7-0:00:00 \
+--job-name=rclone.${pop} --output=logs/rclone.${pop}.%A.out --error=logs/rclone.${pop}.%A.err \
+12b_rclone.sh ${pop}
+
