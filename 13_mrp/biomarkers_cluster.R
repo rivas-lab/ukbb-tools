f0cd2d44789cde3cfa7a73310f73578c7e2ca4ae
Guhan Ram Venkataraman <guhan@sh02-ln01.stanford.edu>
2021-02-21 18:34:59 -0800

    adjsuted biomarkers clustering

--- 13_mrp/biomarkers_cluster.R
+++ 13_mrp/biomarkers_cluster.R
@@ -1,38 +1,34 @@
 library("tidyverse")
 library("comprehenr")
 
-setwd("~/Downloads/")
+#setwd("~/Downloads/")
 
-corrs <- read.delim("corrs_names.tsv", sep="\t", header=TRUE, fill=TRUE, stringsAsFactors=FALSE) %>% 
-  inner_join(read.delim("betalens_names.tsv", sep="\t", header=TRUE, fill=TRUE, stringsAsFactors=FALSE))
-corrs <- corrs %>% filter(PHEN_1 != PHEN_2)
-final_phenos <- read.delim("final_phe_codes.txt", sep="\t", header=FALSE, fill=TRUE, stringsAsFactors=FALSE)
-phenos_to_include <- corrs %>% filter(PHEN_1 %in% final_phenos$V1) %>% select(PHEN_1, shortname_1) %>% unique() %>% rename(PHEN = PHEN_1, shortname = shortname_1)
-corrs <- corrs %>% filter(PHEN_1 %in% phenos_to_include$PHEN) %>% filter(PHEN_2 %in% phenos_to_include$PHEN) %>% filter(BETALEN_NOHLA >= 10)
-
-pearson_nohla_corrs_sig <- corrs %>% filter(PEARSON_P_NOHLA <= 5*10^-8)
-pearson_nohla_corrs_sig_phens <- unique(c(pearson_nohla_corrs_sig$PHEN_1, pearson_nohla_corrs_sig$PHEN_2))
-pearson_nohla_corrs_sig <- pearson_nohla_corrs_sig %>% filter(PHEN_1 %in% pearson_nohla_corrs_sig_phens) %>% filter(PHEN_2 %in% pearson_nohla_corrs_sig_phens)
-pearson_nohla_R_phen <- matrix(0L, nrow = length(pearson_nohla_corrs_sig_phens), ncol = length(pearson_nohla_corrs_sig_phens))
-
-for (row in 1:nrow(pearson_nohla_corrs_sig)) {
-  i = match(c(pearson_nohla_corrs_sig[row, "PHEN_1"]), pearson_nohla_corrs_sig_phens)
-  j = match(c(pearson_nohla_corrs_sig[row, "PHEN_2"]), pearson_nohla_corrs_sig_phens)
-  pearson_nohla_R_phen[i, j] = pearson_nohla_corrs_sig[row, "PEARSON"]
+corrs <- read.delim("biomarkers_array_rg.tsv", sep="\t", header=TRUE, fill=TRUE, stringsAsFactors=FALSE)
+sumstats <- read.delim("sumstat_paths.tsv", sep="\t", header=TRUE, fill=TRUE, stringsAsFactors=FALSE)
+phens <- sumstats$GBE_ID
+R_phen <- matrix(0L, nrow = length(phens), ncol = length(phens))
+print(phens)
+for (row in 1:nrow(corrs)) {
+  i = match(c(corrs[row, "p1"]), phens)
+  j = match(c(corrs[row, "p2"]), phens)
+  R_phen[i, j] = abs(corrs[row, "rg"])
 }
 
-for (row in 1:length(pearson_nohla_corrs_sig_phens)) {
-  for (col in 1:length(pearson_nohla_corrs_sig_phens)) {
+for (row in 1:length(phens)) {
+  for (col in 1:length(phens)) {
     if (row > col) {
-      pearson_nohla_R_phen[row, col] = pearson_nohla_R_phen[col, row]
+      R_phen[row, col] = R_phen[col, row]
+    } else if (row == col) {
+      R_phen[row, col] = 1
     }
   }
 }
 
-pearson_nohla_distances <- dist(pearson_nohla_R_phen)
-dist_hclust <- hclust(pearson_nohla_distances, method = "ward.D2")
+distances <- dist(R_phen)
+dist_hclust <- hclust(distances, method = "ward.D2")
 plot(dist_hclust)
-rect_hclust <- rect.hclust(dist_hclust, k = 100, border="red")
+rect_hclust <- rect.hclust(dist_hclust, border="red")
+
 count = 0
 list1 = c()
 list2 = c()
@@ -43,7 +39,7 @@ for (i in 1:length(rect_hclust)) {
     list2[count] <- i
   }
 }
-pearson_nohla_clusters <- data.frame("PHEN" = to_vec(for(i in 1:length(list1)) pearson_nohla_corrs_sig_phens[list1[i]]), cluster = list2) %>% inner_join(phenos_to_include)
+pearson_nohla_clusters <- data.frame("PHEN" = to_vec(for(i in 1:length(list1)) corrs_phens[list1[i]]), cluster = list2) %>% inner_join(phenos_to_include)
 
 clusters <- pearson_nohla_clusters %>% inner_join(phenos_to_include)
 
@@ -55,3 +51,4 @@ write.table(cluster_sizes, file='clusters_to_run.tsv', quote=FALSE, sep='\t', ro
 
 corrs_nohla_sig <- corrs %>% filter(PEARSON_P_NOHLA <= 5*10^-8) %>% select(PHEN_1, shortname_1, PHEN_2, shortname_2, PEARSON_NOHLA, PEARSON_P_NOHLA, BETALEN_NOHLA)
 write.table(corrs_nohla_sig, file='corrs_pearson_nohla_sig.tsv', quote=FALSE, sep='\t', row.names=FALSE)
+"""
