78a8376798acf70e9963195f4abe1d181ba907f7
Guhan Ram Venkataraman <guhan@sh03-ln07.stanford.edu>
2021-03-04 13:48:48 -0800

    remade biomarkers

--- 13_mrp/mrp_rv_ma_array.sh
+++ 13_mrp/mrp_rv_ma_array.sh
@@ -3,8 +3,8 @@
 #SBATCH --output=mrp_logs/mrp_rv_ma_array.%A_%a.out
 #SBATCH --nodes=1
 #SBATCH --cores=8
-#SBATCH --mem=32000
-#SBATCH --time=04:00:00
+#SBATCH --mem=128000
+#SBATCH --time=2-00:00:00
 
 # define functions
 usage () {
@@ -34,38 +34,46 @@ start_idx=$1
 output_folder=$2
 this_idx=$_SLURM_ARRAY_TASK_ID
 
-min_N_count=100
-GBE_ID=$(cat ../05_gbe/array-combined/phenotype_info.tsv | awk -v min_N=${min_N_count} 'NR > 1 && $8 >= min_N' | awk -v start_idx=$start_idx -v this_idx=$this_idx 'NR==(start_idx + this_idx - 1) {print $1}' )
+GBE_ID=$(grep adjusted ../05_gbe/array-combined/phenotype_info.tsv | grep -v BIN | awk -v start_idx=$start_idx -v this_idx=$this_idx 'NR==(start_idx + this_idx - 1) {print $1}')
 
-echo -e "path\tstudy\tpheno\tR_phen" > $output_folder/$GBE_ID.tmp.txt
+awk -F'\t' -v GBE=${GBE_ID} '{if (($1 == GBE) && ($16 != "NA") && ($16 <= 3) && ($8 >= 1000) && ($4 >= 100000)) {print}}' /oak/stanford/groups/mrivas/ukbb24983/array-combined/gwas/current/gwas.qc-SE02.tsv | grep -v metal | grep -v e_asian | cut -f2 > $output_folder/$GBE_ID.POP_INCLUDE
+
 echo -e "$GBE_ID" >&1
 
 line_phenotype="$(awk -F'\t' -v GBE=${GBE_ID} '{if ($1 == GBE) {print}}' ../05_gbe/array-combined/phenotype_info.tsv)"
 
-echo -e "white_british\nafrican\ne_asian\ns_asian\nnon_british_white\nrelated\nothers" > $GBE_ID.POP
+echo -e "white_british\nafrican\ns_asian\nnon_british_white\nrelated\nothers" > $output_folder/$GBE_ID.POP
 
 N_GBE="$(echo $line_phenotype | cut -d' ' -f8)"
 N_NBW="$(echo $line_phenotype | cut -d' ' -f9)"
 N_AFR="$(echo $line_phenotype | cut -d' ' -f10)"
-N_EAS="$(echo $line_phenotype | cut -d' ' -f11)"
 N_SAS="$(echo $line_phenotype | cut -d' ' -f12)"
 N_REL="$(echo $line_phenotype | cut -d' ' -f13)"
 N_OTH="$(echo $line_phenotype | cut -d' ' -f14)"
 
-echo -e "$N_GBE\n$N_AFR\n$N_EAS\n$N_SAS\n$N_NBW\n$N_REL\n$N_OTH" > $GBE_ID.POP_NUM
+echo -e "$N_GBE\n$N_AFR\n$N_SAS\n$N_NBW\n$N_REL\n$N_OTH" > $output_folder/$GBE_ID.POP_NUM
 
-paste $GBE_ID.POP $GBE_ID.POP_NUM | while read POP NUM; do
+rm $output_folder/$GBE_ID.tmp.txt
+touch $output_folder/$GBE_ID.tmp.txt
+
+paste $output_folder/$GBE_ID.POP $output_folder/$GBE_ID.POP_NUM | while read POP NUM; do
     lines=$(find /oak/stanford/groups/mrivas/ukbb24983/array-combined/gwas/current/ -name "*.$GBE_ID.*gz" | grep -v freeze | grep -v old | grep -v ldsc | grep $POP | wc -l)
-    if [ $lines -eq 1 ] && [ $NUM -ge $min_N_count ]; then
+    if [ $lines -eq 1 ]; then
         PATH_TO_FILE=$(find /oak/stanford/groups/mrivas/ukbb24983/array-combined/gwas/current/ -name "*.$GBE_ID.*gz" | grep -v freeze | grep -v old | grep -v ldsc | grep $POP)
         echo -e "$PATH_TO_FILE\t$POP\t$GBE_ID\tTRUE" >> $output_folder/$GBE_ID.tmp.txt
     fi
 done
 
+grep -Fwf $output_folder/$GBE_ID.POP_INCLUDE $output_folder/$GBE_ID.tmp.txt > $output_folder/$GBE_ID.tmp && mv $output_folder/$GBE_ID.tmp $output_folder/$GBE_ID.tmp.txt
+
+rm $output_folder/$GBE_ID.POP_INCLUDE
+rm $output_folder/$GBE_ID.POP_NUM
+rm $output_folder/$GBE_ID.POP
+
+sed -i '1s/^/path\tstudy\tpheno\tR_phen\n/' $output_folder/$GBE_ID.tmp.txt
+
 cat $output_folder/$GBE_ID.tmp.txt
 
-/share/software/user/open/python/3.6.1/bin/python3 mrp_production.py --file $output_folder/$GBE_ID.tmp.txt --R_study independent similar --R_var independent similar --variants ptv pav --metadata_path /oak/stanford/groups/mrivas/ukbb24983/cal/pgen/ukb_cal-consequence_wb_maf_gene_ld_indep_mpc_pli.tsv --out_folder $output_folder
+/share/software/user/open/python/3.6.1/bin/python3 mrp_production.py --file $output_folder/$GBE_ID.tmp.txt --R_study independent similar --R_var independent similar --variants ptv pav --sigma_m_types sigma_m_mpc_pli --filter_ld_indep --se_thresh 100 --maf_thresh 0.01 --metadata_path /oak/stanford/groups/mrivas/ukbb24983/cal/pgen/ukb_cal-consequence_wb_maf_gene_ld_indep_mpc_pli.tsv --out_folder $output_folder
 
 rm $output_folder/$GBE_ID.tmp.txt
-rm $GBE_ID.POP_NUM
-rm $GBE_ID.POP
